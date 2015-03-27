94ac2b7274be8359fa93c612d0a93c2a34a35c8f
Whitney Young <wbyoung@fadingred.com>
2015-03-27 10:39:42 -0700

    Better test coverage.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,68 @@
 var expect = require('chai').expect;
 var plugin = require('..');
+var sinon = require('sinon');
+var child_process = require('child_process');
 
 describe('plugin', function() {
+
+  beforeEach(function() {
+    var spawn = child_process.spawn;
+    sinon.stub(child_process, 'spawn', function(cmd, args) {
+      return spawn('echo', ['v0.7.12\n0.10.26\nv0.10.28\nv0.10.29\nv0.10.101\nv0.11.13']);
+    });
+  });
+  afterEach(function() { child_process.spawn.restore(); });
+
+  it('matches exact version', function(done) {
+    plugin.match('0.11.13').then(function(result) {
+      expect(result).to.eql({
+        version: 'v0.11.13',
+        command: 'nvm use v0.11.13 > /dev/null;'
+      });
+    })
+    .done(done);
+  });
+
+  it('matches with semver syntax', function(done) {
+    plugin.match('>=0.10 < 0.10.29').then(function(result) {
+      expect(result).to.eql({
+        version: 'v0.10.28',
+        command: 'nvm use v0.10.28 > /dev/null;'
+      });
+    })
+    .done(done);
+  });
+
+  it('chooses greatest match', function(done) {
+    plugin.match('0.10').then(function(result) {
+      expect(result).to.eql({
+        version: 'v0.10.101',
+        command: 'nvm use v0.10.101 > /dev/null;'
+      });
+    })
+    .done(done);
+  });
+
+  it('rejects versions not installed', function(done) {
+    plugin.match('0.9').then(
+      function() { throw new Error('Plugin should have rejected invalid version.'); },
+      function(e) { expect(e).to.eql('no version matching 0.9'); })
+    .done(done);
+  });
+
+  it('rejects when command fails', function(done) {
+    child_process.spawn.restore();
+    var spawn = child_process.spawn;
+    sinon.stub(child_process, 'spawn', function(cmd, args) {
+      return spawn('ls', ['/nowhere']); // intentional command failure
+    });
+
+    plugin.match('0.9').then(
+      function() { throw new Error('Plugin should have rejected bad command.'); },
+      function(e) { expect(e).to.match(/nvm exited with status: \d+/); })
+    .done(done);
+  });
+
   it('parses nvm@dc53a37 output', function() {
     var output = {
       stdout: new Buffer('   v0.7.12\n   v0.8.26\n   v0.9.12\n  v0.10.26\n  v0.10.28\n  v0.11.11\n  v0.11.12\n  v0.11.13\ncurrent: \tv0.10.26\n0.10 -> 0.10.26 (-> v0.10.26)\ndefault -> 0.10 (-> v0.10.26)\n')
